32f51c3cdba4ba86f54a96f267c81a647250d109
marcinwardawa <marcin.wardawa@hotmail.com>
2023-05-17 21:12:59 +0200

    Added end point Get Services

---
+++ backend/Car-Detailing-Backend/Controllers/CarDetailingController.cs
@@ -0,0 +1,55 @@
+﻿using Car_Detailing_Backend.Data;
+using Car_Detailing_Backend.Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Car_Detailing_Backend.Controllers
+{
+    [ApiController]
+    [Route("[action]")]
+    [Produces("application/json")]
+    public class CarDetailingController : ControllerBase
+    {
+        private readonly ILogger<CarDetailingController> logger;
+
+        public CarDetailingController(ILogger<CarDetailingController> logger)
+        {
+            this.logger = logger;
+        }
+
+
+        /// <summary>
+        /// Get Services
+        /// </summary>
+        /// <returns>List of services</returns>
+        /// <response code="200">Ok</response>
+        /// <remarks>
+        /// {
+        ///     “id”: 1,
+        ///     “name”: “Mycie ręczne”,
+        ///     “type”: “Mycie zewnętrzne”,
+        ///     “price”: 50,
+        ///     }
+        ///</remarks>
+        /// <response code="400">BadRequest</response>
+        [HttpGet(Name = "/services")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<List<list_of_services>>> Services()
+        {
+            try
+            {
+                var data = new data_reading();
+                var dane = await data.readDataAsyns();
+                return Ok(dane);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Błąd działania metdoy Get Service");
+                logger.LogError(ex.ToString());
+                return BadRequest();
+            }
+        }
+
+    }
+}
---
+++ backend/Car-Detailing-Backend/Data/data_reading.cs
@@ -0,0 +1,36 @@
+﻿using Car_Detailing_Backend.Data.Models;
+using Microsoft.VisualBasic.FileIO;
+using System;
+
+namespace Car_Detailing_Backend.Data
+{
+    public class data_reading
+    {
+        private readonly string path = @"./Data/data_files/serviceData.csv";
+        List<list_of_services> list = new List<list_of_services>();
+
+        public async Task<List<list_of_services>> readDataAsyns()
+        {
+            try
+            {
+                var line = await File.ReadAllLinesAsync(path);
+                foreach (var row in line)
+                {
+                    var price_list_obj = new list_of_services();
+                    var text = row.Split(',');
+                    price_list_obj.service_id = int.Parse(text[0]);
+                    price_list_obj.name = text[1];
+                    price_list_obj.type = text[2];
+                    price_list_obj.price = double.Parse(text[3]);
+                    list.Add(price_list_obj);
+                }
+
+                return list;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
