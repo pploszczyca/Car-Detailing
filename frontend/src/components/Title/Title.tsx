950f6d9d63a5bce65dfd676f397230a436336e59
pploszczyca <piotrus.plo@gmail.com>
2023-05-18 17:42:37 +0200

    Prepaired main page

---
+++ frontend/src/components/Title/Title.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-family: 'Literata', serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 32px;
+  line-height: 48px;
+  /* identical to box height */
+
+  color: #000000;
+`
+
+export default Title
---
+++ frontend/src/pages/Main/MainPageComponents.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 16vw;
+  margin-top: 10vh;
+`;
+
+export const HorizontalLine = styled.hr`
+  width: 98px;
+  border: 1px solid #000000;
+  margin: 0 24px;
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 36px;
+`;
+
+export const LogoImage = styled.img`
+  display: block;
+  margin: 17vh auto 0 auto;
+`;
