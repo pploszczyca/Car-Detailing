d8e84efc15fabea031bc735e27add105540c2c46
pploszczyca <piotrus.plo@gmail.com>
2023-05-18 14:10:29 +0200

    Added logo.png

--- frontend/src/pages/Main/MainPage.tsx
+++ frontend/src/pages/Main/MainPage.tsx
@@ -1,11 +1,13 @@
 import useMainPage from "./useMainPage.ts";
 import Button from "../../components/Button/Button.tsx";
+import logo from "../../assets/images/logo.png"
 
 export function MainPage() {
     const {onMakeAnOrderClicked, onOrdersClicked} = useMainPage()
 
     return (
         <div>
+            <img src={logo} alt="Logo"/>
             <Button onClick={onMakeAnOrderClicked}>Umów wizytę w salonie</Button>
             <Button onClick={onOrdersClicked}>Sprawdź swoje wizyty</Button>
         </div>
