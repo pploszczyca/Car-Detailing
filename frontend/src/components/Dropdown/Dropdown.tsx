4626aca00e39cfafbeede081b7716157b46ede44
pploszczyca <piotrus.plo@gmail.com>
2023-05-22 21:40:52 +0200

    Changed Dropdown to has arrow

--- frontend/src/components/Dropdown/Dropdown.tsx
+++ frontend/src/components/Dropdown/Dropdown.tsx
@@ -1,27 +1,20 @@
 import styled from 'styled-components';
 
-const Dropdown = styled.select`
+export const DropdownWrapper = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+export const Dropdown = styled.select`
   background: rgba(249, 168, 38, 0.15);
   border: 1px solid #F9A826;
   box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 20px;
   appearance: none;
   padding: 10px;
+  width: 100%;
 
-  &:after {
-    content: '▼';
-    font-size: 10px;
-    position: absolute;
-    right: 10px;
-    top: 50%;
-    transform: translateY(-50%) matrix(-1, -0.02, -0.02, 1, 0, 0);
-    pointer-events: none;
-    background: #F9A826;
-    border-radius: 3px;
-    padding: 2px;
-  }
-
-  font-family: 'Literata',serif;
+  font-family: 'Literata', serif;
   font-style: normal;
   font-weight: 400;
   font-size: 32px;
@@ -30,4 +23,14 @@ const Dropdown = styled.select`
   color: #000000;
 `;
 
-export default Dropdown;
+export const DropdownArrow = styled.div`
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  pointer-events: none;
+  background: #F9A826;
+  width: 32px;
+  height: 32px;
+  clip-path: polygon(100% 0%, 0 0%, 50% 100%);
+`;
--- frontend/src/pages/SelectLocation/SelectLocationPage.tsx
+++ frontend/src/pages/SelectLocation/SelectLocationPage.tsx
@@ -1,17 +1,20 @@
 import {ServicesContainerWithNavbar} from "../../components/Navbar/ContainerWithNavbar.tsx";
 import Title from "../../components/Title/Title.tsx";
-import Dropdown from "../../components/Dropdown/Dropdown.tsx";
+import {Dropdown, DropdownArrow, DropdownWrapper} from "../../components/Dropdown/Dropdown.tsx";
 import DropdownOption from "../../components/Dropdown/DropdownOption.tsx";
 
 const SelectLocationPage = () => {
     return <ServicesContainerWithNavbar>
         <Title>Hello from SelectLocation!!!</Title>
 
-        <Dropdown>
-            <DropdownOption>Option 1</DropdownOption>
-            <DropdownOption>Option 2</DropdownOption>
-            <DropdownOption>Option 3</DropdownOption>
-        </Dropdown>
+        <DropdownWrapper>
+            <Dropdown>
+                <DropdownOption>Option 1</DropdownOption>
+                <DropdownOption>Option 2</DropdownOption>
+                <DropdownOption>Option 3</DropdownOption>
+            </Dropdown>
+            <DropdownArrow />
+        </DropdownWrapper>
     </ServicesContainerWithNavbar>
 }
 
