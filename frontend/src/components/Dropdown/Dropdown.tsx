cd0ba42cde777f87b5dbb9ced3b74e46dec27e6d
pploszczyca <piotrus.plo@gmail.com>
2023-05-22 21:54:34 +0200

    Created DropdownComponent

--- frontend/src/components/Dropdown/Dropdown.tsx
+++ frontend/src/components/Dropdown/Dropdown.tsx
@@ -1,17 +1,19 @@
 import styled from 'styled-components';
+import React, { useState } from 'react';
 
-export const DropdownWrapper = styled.div`
+const DropdownWrapper = styled.div<{ width: string }>`
   position: relative;
   display: inline-block;
+  width: ${({ width }) => width};
 `;
 
-export const Dropdown = styled.select`
+const Dropdown = styled.select<{ isOpen: boolean }>`
   background: rgba(249, 168, 38, 0.15);
   border: 1px solid #F9A826;
   box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 20px;
   appearance: none;
-  padding: 10px;
+  padding: 2px;
   width: 100%;
 
   font-family: 'Literata', serif;
@@ -23,14 +25,43 @@ export const Dropdown = styled.select`
   color: #000000;
 `;
 
-export const DropdownArrow = styled.div`
+const DropdownArrow = styled.div<{ isOpen: boolean }>`
   position: absolute;
   right: 10px;
   top: 50%;
-  transform: translateY(-50%);
+  transform: translateY(-50%) ${({ isOpen }) => isOpen ? 'rotate(180deg)' : 'rotate(0deg)'};
   pointer-events: none;
   background: #F9A826;
   width: 32px;
   height: 32px;
   clip-path: polygon(100% 0%, 0 0%, 50% 100%);
 `;
+
+interface Props {
+    onOptionChanged: (value: string) => void;
+    children: React.ReactNode;
+    width: string;
+}
+
+export const DropdownComponent: React.FC<Props> = ({ onOptionChanged, width, children }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        onOptionChanged(event.target.value);
+    };
+
+    return (
+        <DropdownWrapper onClick={handleToggle} width={width}>
+            <Dropdown onChange={handleChange} isOpen={isOpen}>
+                {children}
+            </Dropdown>
+            <DropdownArrow isOpen={isOpen} />
+        </DropdownWrapper>
+    );
+};
+
+export default DropdownComponent;
--- frontend/src/pages/SelectLocation/SelectLocationPage.tsx
+++ frontend/src/pages/SelectLocation/SelectLocationPage.tsx
@@ -1,20 +1,17 @@
 import {ServicesContainerWithNavbar} from "../../components/Navbar/ContainerWithNavbar.tsx";
 import Title from "../../components/Title/Title.tsx";
-import {Dropdown, DropdownArrow, DropdownWrapper} from "../../components/Dropdown/Dropdown.tsx";
 import DropdownOption from "../../components/Dropdown/DropdownOption.tsx";
+import DropdownComponent from "../../components/Dropdown/Dropdown.tsx";
 
 const SelectLocationPage = () => {
     return <ServicesContainerWithNavbar>
         <Title>Hello from SelectLocation!!!</Title>
 
-        <DropdownWrapper>
-            <Dropdown>
-                <DropdownOption>Option 1</DropdownOption>
-                <DropdownOption>Option 2</DropdownOption>
-                <DropdownOption>Option 3</DropdownOption>
-            </Dropdown>
-            <DropdownArrow />
-        </DropdownWrapper>
+        <DropdownComponent width={"50vw"} onOptionChanged={() => {}}>
+            <DropdownOption>Option 1</DropdownOption>
+            <DropdownOption>Option 2</DropdownOption>
+            <DropdownOption>Option 3</DropdownOption>
+        </DropdownComponent>
     </ServicesContainerWithNavbar>
 }
 
